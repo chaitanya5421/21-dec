6e7cf0ddf2ce92e4ce43a42f18591c8e0144031c
chaitanya5421 <chaithanya5421@gmail.com>
2024-04-10 16:54:20 +0530

    react-

--- react/21-reactapp/src/App.js
+++ react/21-reactapp/src/App.js
@@ -4,6 +4,7 @@ import './App.css'
 import Counter from "./Components/Counter"
 import Stylings from "./Components/Stylings/Stylings"
 import MountingComp from "./Components/MountingComp"
+import Pagination from "./Components/Pagination"
 
 const App = () => {
   // props 
@@ -16,7 +17,8 @@ const App = () => {
       {/* <ClassComp phoneName="iphone" /> */}
       {/* <Counter /> */}
       {/* <Stylings /> */}
-      <MountingComp />
+      {/* <MountingComp /> */}
+      <Pagination />
     </center>
   )
 }
---
+++ react/21-reactapp/src/Components/Pagination.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+
+class Pagination extends Component {
+
+    state = {
+        usersData: [],
+        pageNumber:0
+    }
+
+    componentDidMount(){
+        this.getUsersData()
+    }
+
+    getUsersData = async () => {
+        const response = await fetch(`https://dummyapi.io/data/v1/user?page=${this.state.pageNumber}&limit=10`, {
+            method: 'GET',
+            headers: {
+                "app-id": '633e33e98efd49504c9c7643'
+            }
+        });
+
+        const data = await response.json()
+        console.log(data)
+        this.setState({usersData: data})
+    }
+
+  render() {
+    return (
+      <div>
+        <h2>Pagination</h2>
+        
+      </div>
+    )
+  }
+}
+
+export default Pagination
